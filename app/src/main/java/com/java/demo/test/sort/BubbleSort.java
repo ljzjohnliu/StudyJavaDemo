7073f106f2f7670136a6998f9259559d34fd3109
liujianzhang <liujianzhang@qiyi.com>
2020-10-29 19:12:11 +0800

    冒泡排序法

---
+++ app/src/main/java/com/java/demo/test/sort/BubbleSort.java
@@ -0,0 +1,46 @@
+package com.java.demo.test.sort;
+
+public class BubbleSort {
+
+    /**
+     * 算法描述
+     * 冒泡排序算法的算法过程如下：
+     * ①. 比较相邻的元素。如果第一个比第二个大，就交换他们两个。
+     * ②. 对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对。这步做完后，最后的元素会是最大的数。
+     * ③. 针对所有的元素重复以上的步骤，除了最后一个。
+     * ④. 持续每次对越来越少的元素重复上面的步骤①~③，直到没有任何一对数字需要比较。
+     *
+     * @param array
+     */
+    public static void sort(int[] array) {
+        if (array == null || array.length == 0) {
+            return;
+        }
+
+        int length = array.length;
+        //外层：需要length-1次循环比较
+        for (int i = 0; i < length - 1; i++) {
+            //内层：每次循环需要两两比较的次数，每次比较后，都会将当前最大的数放到最后位置，所以每次比较次数递减一次
+            for (int j = 0; j < length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    //交换数组array的j和j+1位置的数据
+                    swap(array, j, j + 1);
+                }
+
+            }
+        }
+    }
+
+    /**
+     * 交换数组array的i和j位置的数据
+     *
+     * @param array 数组
+     * @param i     下标i
+     * @param j     下标j
+     */
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
---
+++ app/src/main/java/com/java/demo/test/sort/SortTest.java
@@ -0,0 +1,33 @@
+package com.java.demo.test.sort;
+
+import java.util.Arrays;
+
+public class SortTest {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{4, 3, 7, 5, 10, 9, 1, 6, 8, 2};
+        /**
+         * 数组的输出的三种方式
+         *         一维数组：
+         *         定义一个数组   int[] array = {1,2,3,4,5};
+         * （1）传统的for循环方式
+         *         for(int i=0;i<array.length;i++)
+         *         {
+         *             System.out.println(array[i]);
+         *         }
+         * （2）for each循环
+         *         for(int a:array)
+         *             System.out.println(a);
+         * （3）利用Array类中的toString方法
+         *         调用Array.toString(a)，返回一个包含数组元素的字符串，这些元素被放置在括号内，并用逗号分开
+         *         int[] array = {1,2,3,4,5};
+         *         System.out.println(Arrays.toString(array));
+         *         输出：[1, 2, 3, 4, 5]
+         *         说明：System.out.println(array);这样是不行的，这样打印是的是数组的首地址。
+         */
+
+        System.out.println(Arrays.toString(array));
+        BubbleSort.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
